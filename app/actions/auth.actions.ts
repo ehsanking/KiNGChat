a7f20b5c5a1d9aa6ad21f4644c9273bd5019ffdb
Ehsan <1883051+ehsanking@users.noreply.github.com>
2026-04-08 22:10:21 +0200

    fix(prisma): add offline-compatible validate script

--- app/actions/auth.actions.ts
+++ app/actions/auth.actions.ts
@@ -12,7 +12,7 @@
 
 import { loginUser as origLoginUser } from './auth.login';
 import { registerUser as origRegisterUser } from './auth.register';
-import { getPublicSettings as origGetPublicSettings } from './auth-legacy';
+import { getPublicSettings as origGetPublicSettings, updateAdminCredentials as origUpdateAdminCredentials } from './auth-legacy';
 
 export async function registerUser(...args: Parameters<typeof origRegisterUser>) {
   return origRegisterUser(...args);
@@ -24,4 +24,8 @@ export async function loginUser(...args: Parameters<typeof origLoginUser>) {
 
 export async function getPublicSettings(...args: Parameters<typeof origGetPublicSettings>) {
   return origGetPublicSettings(...args);
+}
+
+export async function updateAdminCredentials(...args: Parameters<typeof origUpdateAdminCredentials>) {
+  return origUpdateAdminCredentials(...args);
 }
